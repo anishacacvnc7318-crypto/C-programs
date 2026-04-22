bf4db12798415521bd8c32e692cb596c8a79af96
anishacacvnc7318-crypto <anishacacvnc7318@gmail.com>
2026-04-22 12:25:07 +0530

    Create largest.c

---
+++ largest.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+int a,b,largest;
+printf("Enter first number:");
+scanf("%d",&a);
+printf("Enter second number:");
+scanf("%d",&a);
+if(a>b)
+printf("Largest=%d",a);
+else
+printf("Largest=%d",b);
+}
+
+
